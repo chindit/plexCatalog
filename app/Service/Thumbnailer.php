cd8220f4b55923f023a298e1834e97af1b888ed6
chindit <littletiger58@gmail.com>
2024-01-10 11:27:13 +0100

    Return empty string for client exception on thumbnails

--- app/Service/Thumbnailer.php
+++ app/Service/Thumbnailer.php
@@ -5,15 +5,21 @@ namespace App\Service;
 
 use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\ImageManager;
+use Symfony\Component\HttpClient\Exception\ClientException;
 use Symfony\Component\HttpClient\HttpClient;
 
 final class Thumbnailer
 {
     public function thumbnail(string $url): string
     {
+        try {
+            $thumb = HttpClient::create()->request('GET', $url);
+        } catch (ClientException $exception) {
+            return '';
+        }
         $newName = tempnam(sys_get_temp_dir(), 'plex_thumb_') . '.jpg';
         $resizedName = tempnam(sys_get_temp_dir(), 'plex_thumb_resized_') . '.jpg';
-        file_put_contents($newName, HttpClient::create()->request('GET', $url)->getContent());
+        file_put_contents($newName, $thumb->getContent());
 
         $manager = new ImageManager(new Driver());
         $manager->read($newName)->scale(width: 150)->save($resizedName);
