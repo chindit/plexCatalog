b74a0a2e4edcc1f9e1c55b5d011f498c7a595e0a
David <littletiger58@gmail.com>
2026-08-27 17:46:18 +0200

    Update dependencies in composer.lock

--- app/Service/Thumbnailer.php
+++ app/Service/Thumbnailer.php
@@ -6,6 +6,7 @@ namespace App\Service;
 use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\ImageManager;
 use Symfony\Component\HttpClient\Exception\ClientException;
+use Symfony\Component\HttpClient\Exception\TransportException;
 use Symfony\Component\HttpClient\HttpClient;
 
 final class Thumbnailer
@@ -13,10 +14,11 @@ final class Thumbnailer
     public function thumbnail(string $url): string
     {
         try {
-            $imageContent = HttpClient::create()->request('GET', $url)->getContent();
+            $imageContent = HttpClient::create(['verify_host' => false])->request('GET', $url)->getContent();
         } catch (ClientException $exception) {
             return '';
         }
+
         $newName = tempnam(sys_get_temp_dir(), 'plex_thumb_') . '.jpg';
         $resizedName = tempnam(sys_get_temp_dir(), 'plex_thumb_resized_') . '.jpg';
         file_put_contents($newName, $imageContent);
